5210d09107415414508824427fa99d64704f3aad
italogar6 <italogar6@hotmail.com>
2016-05-09 17:24:15 +0100

    header y defines

---
+++ DescensoRecursivo/recursivedescent.cpp
@@ -0,0 +1,56 @@
+#include "recursivedescent.h"
+int main(int argc, char* argv)
+{
+
+}
+
+void statements()
+{
+
+}
+
+void statement()
+{
+
+}
+
+
+void assignment()
+{
+
+}
+
+void expression()
+{
+
+}
+
+void function()
+{
+
+}
+
+void variables()
+{
+
+}
+
+void bucle_for()
+{
+
+}
+
+void bucle_while()
+{
+
+}
+
+void cond_if()
+{
+
+}
+
+void value()
+{
+
+}
